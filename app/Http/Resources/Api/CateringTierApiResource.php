6c6ac5d66f4c7fe0680d280eef9bb6941a560b6e
Ryan-08 <maskuri.opo8@gmail.com>
2025-10-05 18:24:44 +0700

    Update BE

--- app/Http/Resources/Api/CateringTierApiResource.php
+++ app/Http/Resources/Api/CateringTierApiResource.php
@@ -15,14 +15,15 @@ class CateringTierApiResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id, 
-            'name' => $this->name, 
+            'id' => $this->id,
+            'name' => $this->name,
             'price' => $this->price,
             'quantity' => $this->quantity,
             'duration' => $this->duration,
             'photo' => $this->photo,
-            'cateringPackage' => new CateringPackageApiResource($this->whenLoaded('cateringPackage')), 
-            'benefits' => TierBenefitApiResource::collection ($this->whenLoaded ('benefits')),
+            'tagline' => $this->tagline,
+            'cateringPackage' => new CateringPackageApiResource($this->whenLoaded('cateringPackage')),
+            'benefits' => TierBenefitApiResource::collection($this->whenLoaded('benefits')),
         ];
     }
 }
